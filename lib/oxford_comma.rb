db7fe86120fa558164e552c3f70b062d17669fa1
Nathan Steed <steedn@outlook.com>
2020-07-30 00:19:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.size == 2
+      array.join(" and ")
+  elsif array.size == 1
+    array.join
+  else
+  array[-1].insert(0, "and ")
+  array.join(", ")
+  end
 end
